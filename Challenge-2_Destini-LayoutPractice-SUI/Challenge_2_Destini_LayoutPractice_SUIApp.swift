5b174a3a64fdf46f5ffd3ed5b0f8ea11dcacd2cc
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-02-02 19:36:34 +0300

    Initial Commit

---
+++ Challenge-2_Destini-LayoutPractice-SUI/Challenge_2_Destini_LayoutPractice_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2_Destini_LayoutPractice_SUIApp.swift
+//  Challenge-2_Destini-LayoutPractice-SUI
+//
+//  Created by Андрей Бородкин on 02.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2_Destini_LayoutPractice_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge-2_Destini-LayoutPractice-SUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Challenge-2_Destini-LayoutPractice-SUI
+//
+//  Created by Андрей Бородкин on 02.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
